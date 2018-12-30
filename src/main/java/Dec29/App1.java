811125555059ca7d0b0132aae12df72dfc4ae6bc
GSNarayana <gsng518@gmail.com>
2018-12-30 09:44:03 +0530

    Feature 2.0 is updated

---
+++ src/main/java/Dec29/App1.java
@@ -0,0 +1,13 @@
+package Dec29;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi this is Sankara in Feature 2!" );
+    }
+}
